0af72e17180109a047987977ecbc88bccd66feee
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-11-05 05:00:46 +0100

    Add dark mode

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -2,7 +2,9 @@ import Link from "next/link"
 
 const PostCard = ({ post }) => (
   <Link href={`/blog${post.link}`}>
-    <a>{post.title}</a>
+    <a className="font-medium text-gray-500 hover:text-gray-100 dark:(hover:text-gray-900) transition duration-150 ease-in-out">
+      {post.title}
+    </a>
   </Link>
 )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,67 @@ module.exports = {
   future: "all",
   purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   theme: {
+    typography: (theme) => ({
+      dark: {
+        css: {
+          color: theme("colors.gray[300]"),
+          '[class~="lead"]': {
+            color: theme("colors.gray[400]"),
+          },
+          a: {
+            color: theme("colors.gray[100]"),
+          },
+          strong: {
+            color: theme("colors.gray[100]"),
+          },
+          "ol > li::before": {
+            color: theme("colors.gray[500]"),
+          },
+          "ul > li::before": {
+            backgroundColor: theme("colors.gray[700]"),
+          },
+          hr: {
+            borderColor: theme("colors.gray[800]"),
+          },
+          blockquote: {
+            color: theme("colors.gray[100]"),
+            borderLeftColor: theme("colors.gray[800]"),
+          },
+          h1: {
+            color: theme("colors.gray[100]"),
+          },
+          h2: {
+            color: theme("colors.gray[100]"),
+          },
+          h3: {
+            color: theme("colors.gray[100]"),
+          },
+          h4: {
+            color: theme("colors.gray[100]"),
+          },
+          "figure figcaption": {
+            color: theme("colors.gray[500]"),
+          },
+          code: {
+            color: theme("colors.gray[100]"),
+          },
+          pre: {
+            color: theme("colors.gray[800]"),
+            backgroundColor: theme("colors.gray[200]"),
+          },
+          thead: {
+            color: theme("colors.gray[100]"),
+            borderBottomColor: theme("colors.gray[700]"),
+          },
+        },
+      },
+    }),
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        dark: { raw: "(prefers-color-scheme: dark)" },
       },
     },
   },
